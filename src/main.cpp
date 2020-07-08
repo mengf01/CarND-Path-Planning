08e86a5843389c34c580b7376ff4c86fdffd27da
mengf01 <meng.fan.xy@gmail.com>
2020-07-07 21:30:31 -0700

    Finish lane change

--- src/main.cpp
+++ src/main.cpp
@@ -56,10 +56,15 @@ int main() {
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // initialization of variables across the whole process
-    int lane = 1; // start on lane 1
-    double ref_vel = 49.5; // mph
+    static int lane = 1; // start on lane 1
+    static double ref_vel = 0.0; // mph
     const double MS_PER_MPH = 0.447; 
     const double TIME_STEP = 0.02;
+    // we should not change 2 lanes at one time or change our mind after decided to change lane.
+    // COOL_DOWN will decrease by 1 each time (until 0), and reset to maximum whenever we change lane.
+    // Thus, it will keep lane value unchanged for some time, OOL_DOWN = 75 is roughly 75*(0.02*4)=6 seconds
+    const int COOL_DOWN_MAX = 75;
+    static int cool_down = COOL_DOWN_MAX; 
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -104,7 +109,123 @@ int main() {
            *   sequentially every .02 seconds
            */
           
+          if (cool_down>0){
+            cool_down -= 1;
+          }
+
           int prev_size = previous_path_x.size();
+          if (prev_size>0){
+            car_s = end_path_s;
+          }
+          // too close to the car ahead
+          bool too_close = false;
+          // // changing lane and not there yet
+          // bool changing_lane = false;
+          for (const auto& actor : sensor_fusion){
+            double actor_d = actor[6];
+            // actor in the same lane
+            if (actor_d<(2+4*lane+2) && actor_d>(2+4*lane-2)){
+              double actor_vx = actor[3];
+              double actor_vy = actor[4];
+              double check_speed = sqrt(actor_vx*actor_vx + actor_vy*actor_vy);
+              double check_actor_s = actor[5];
+              // assume the acotr will move at constant velocity
+              check_actor_s += prev_size * TIME_STEP * check_speed;
+              if (check_actor_s>car_s && check_actor_s<(car_s+30)){
+                too_close = true;
+              } 
+            }
+          }
+
+          if (too_close){
+            ref_vel -= 0.22;
+            // It's appealing to change lane when there is a car ahead of us.
+            // If we are currently changing lane (i.e., not close enough to the center of lane to change), skip the process
+            // Here I'd like to change lane if meeting all the following criteria:
+            // 1, ref_vel > LANE_CHANGE_VEL, so that the car can swiftly shift lane
+            // 2, there is at least AHEAD_BUFFER (meters ahead of the ref point) in the available lane
+            // 3, there is at least BEHIND_BUFFER (meters behind current car position) in the available lane
+            // 4, If more than 1 lane is available, choose the lane that the car ahead is farther from us
+            double LANE_CHANGE_VEL = 30;
+            double AHEAD_BUFFER = 40;
+            double BEHIND_BUFFER = 20;
+            if ((ref_vel>LANE_CHANGE_VEL) && (cool_down==0)){
+              vector<bool> is_lane_avail{true, true, true};
+              if (car_d<4){
+                is_lane_avail[0] = false;
+                is_lane_avail[2] = false;
+              }
+              else if (car_d>8){
+                is_lane_avail[2] = false;
+                is_lane_avail[0] = false;
+              }
+              else{
+                is_lane_avail[1] = false;
+              }
+              // a target car closest to us in front of us in the lane to change
+              vector<double> target_actor_s_dif{9999,9999,9999};
+              vector<double> LEFT_BOUND{0,4,8}; // left boundary of lane
+              // in the available lane, find if the buffer is enough to change lane, update target car stats
+              for (const auto& actor : sensor_fusion){
+                double actor_d = actor[6];
+                // loop through lanes (j)
+                for (int j=0; j<3; j++){
+                  if ((is_lane_avail[j]) && (actor_d>LEFT_BOUND[j]) && (actor_d<LEFT_BOUND[j]+4)){
+                    double check_actor_s = actor[5];
+                    double actor_vx = actor[3];
+                    double actor_vy = actor[4];
+                    double check_speed = sqrt(actor_vx*actor_vx + actor_vy*actor_vy);
+                    check_actor_s += prev_size * TIME_STEP * check_speed;
+                    // the lane is not safe to use if the actor is in the buffer
+                    if (check_actor_s>(car_s-BEHIND_BUFFER) && check_actor_s<(car_s+AHEAD_BUFFER)){
+                      is_lane_avail[j] = false;
+                    } else{
+                      // update if the car is good to be a target car
+                      if (check_actor_s-car_s<target_actor_s_dif[j]){
+                        target_actor_s_dif[j] = check_actor_s-car_s;
+                      }
+                    } // end check buffer
+                  } // end if lane available
+                } // end lane loop
+                // actor not in the same lane
+                if (!(actor_d<(2+4*lane+2) && actor_d>(2+4*lane-2))){
+                  double actor_vx = actor[3];
+                  double actor_vy = actor[4];
+                  double check_speed = sqrt(actor_vx*actor_vx + actor_vy*actor_vy);
+                  double check_actor_s = actor[5];
+                  // assume the actor will move at constant velocity
+                  check_actor_s += prev_size * TIME_STEP * check_speed;
+                  if (check_actor_s>car_s && check_actor_s<(car_s+30)){
+                    too_close = true;
+                  } 
+                }
+              }
+              // decide which lane to change
+              int lane_decision = -1;
+              double target_actor_s_dif_decision = 0;
+              for (int j=0; j<3; j++){
+                if (is_lane_avail[j]){
+                  // just 1 candidate
+                  if (lane_decision==-1){
+                    lane_decision = j;
+                    target_actor_s_dif_decision = target_actor_s_dif[j];
+                  } else if (target_actor_s_dif[j]>target_actor_s_dif_decision){ // 2 candidates
+                    lane_decision = j;
+                  }
+                }
+              }
+              // change lane with constant velocity, i.e., add the reduced velocity back
+              if (lane_decision!=-1){
+                lane = lane_decision;
+                ref_vel += 0.22;
+                cool_down = COOL_DOWN_MAX;
+              }
+            }// end if ref_vel>LANE_CHANGE_VEL
+          }// end if too_close
+          else if (ref_vel<49.5){
+            ref_vel += 0.22;
+          }
+
           // Creat a list of waypoints that are widely spaced. I'll use them to do spline fitting
           vector<double> ptsx;
           vector<double> ptsy;
@@ -150,9 +271,6 @@ int main() {
             double local_y = ptsy[i] - ref_y;
             ptsx[i] = local_x * cos(-ref_yaw_rad) - local_y * sin(-ref_yaw_rad);
             ptsy[i] = local_x * sin(-ref_yaw_rad) + local_y * cos(-ref_yaw_rad);
-            std::cout << "x and y\n";
-            std::cout << ptsx[i] << "\n";
-            std::cout << ptsy[i] << "\n";
           }
           tk::spline spl;
           spl.set_points(ptsx, ptsy);
@@ -170,10 +288,7 @@ int main() {
           int NPOINTS = 50;
           // how may points to split the fitted spline
           double N = end_dist / (ref_vel * MS_PER_MPH * TIME_STEP);
-          std::cout << "N\n";
-          std::cout << N;
-          std::cout << "prev_size\n";
-          std::cout << prev_size;
+
           for (int i=0;i<NPOINTS-prev_size;i++){
             double cur_x = (i+1)*end_x/N;
             double cur_y = spl(cur_x);
@@ -182,14 +297,6 @@ int main() {
             double global_cur_y = cur_x * sin(ref_yaw_rad) + cur_y * cos(ref_yaw_rad);
             next_x_vals.push_back(global_cur_x + ref_x);
             next_y_vals.push_back(global_cur_y + ref_y);
-          }
-          std::cout << "next x vals\n";
-          for (int i=0;i<next_x_vals.size();i++){
-            std::cout << next_x_vals[i] << "\n";
-          }
-          std::cout << "next y vals\n";
-          for (int i=0;i<next_x_vals.size();i++){
-            std::cout << next_y_vals[i] << "\n";
           }
 
           msgJson["next_x"] = next_x_vals;
